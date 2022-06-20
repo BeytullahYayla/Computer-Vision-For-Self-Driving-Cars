039ceb2cfa08ff8ac65e5943fce489a731cb2e3e
Beytullah Yayla <beytullah.yayla@ogr.sakarya.edu.tr>
2022-06-20 20:06:14 +0300

    Avarage Slope Intercept Method Added

--- Finding Lane Lines/Detecting_lines/Lines.py
+++ Finding Lane Lines/Detecting_lines/Lines.py
@@ -1,6 +1,36 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+
+def make_coordinates(image,line_parameters):
+    slope,intercept=line_parameters
+    y1=image.shape[0]
+    y2=int(y1*(3/5))
+    x1=int((y1-intercept)/slope)
+    x2=int((y2-intercept)/slope)
+
+    return np.array([x1,y1,x2,y2])
+
+def avarage_slope_intercept(image,lines):
+    left_fit=[]
+    right_fit=[]
+    for line in lines:
+        x1,y1,x2,y2=line.reshape(4)
+        parameters=np.polyfit((x1,x2),(y1,y2),1)
+        slope=parameters[0]
+        intercept=parameters[1]
+        if slope<0:
+            left_fit.append((slope,intercept))
+        else:
+            right_fit.append((slope,intercept))
+        left_fit_avarage=np.average(left_fit,axis=0)
+        right_fit_avarage=np.average(right_fit,axis=0)
+        #avaraging process will be done across the rows
+        left_line=make_coordinates(image,left_fit_avarage)
+        right_line=make_coordinates(image,right_fit_avarage)
+
+        return np.array([left_line,right_line])
+
 def canny(image):
     gray_image=cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
     blurred=cv2.GaussianBlur(gray_image,(5,5),0)
@@ -31,9 +61,11 @@ canny_image=canny(lane_image)
 cropped_image=region_of_interest(canny_image)
 line=cv2.HoughLinesP(cropped_image,2,np.pi/180,100,np.array([]),minLineLength=40,maxLineGap=5)
 line_image=display_lines(lane_image,line)
+
+avaraged_line=avarage_slope_intercept(lane_image,line)
+line_image_with_avarage_slope_intercept=display_lines(lane_image,avaraged_line)
 cv2.imshow("Lines on the image",line_image)
-combo_image=cv2.addWeighted(line_image,0.8,lane_image,1,1)
-cv2.imshow("Image with lines",combo_image)
-
-
+#combo_image=cv2.addWeighted(lane_image,0.8,line_image,1,1)
+#cv2.imshow("Image with lines",combo_image)
+print(lane_image.shape)#(704,1279,3)
 cv2.waitKey(0)
